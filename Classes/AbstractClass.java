4f9baa8acc51d918e89ea65a29498d0d402458fd
Dudekula Hazra Bee <dudekulahazrabee@gmail.com>
2026-02-07 15:02:10 +0530

    Create AbstractClass.java

---
+++ Classes/AbstractClass.java
@@ -0,0 +1,13 @@
+Abstract Class in Java :
+     *  In Java, an Abstract class is defined using the keyword "abstract".
+     *  An abstract class cannot be instantiated (Object cannot be created).
+     *  An abstract class is designed to be extended by another classes or an abstract class.
+     *  An abstract class contains both abstract methods (only declaration - no imlementation) and concrete methods(complete implementations).
+     An abstract class can have all the following :-
+         -----> Abstract methods (methods with a body)
+         -----> Concrete methods (methods with implementation)
+         -----> Constructors
+         -----> Instance Variables
+         -----> Static and final methods - not abstract (must be implemented)
+  *  In Java, we can achieve partial abstraction using abstract classes.
+  
