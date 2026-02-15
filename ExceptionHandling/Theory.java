05b16c839b481384d899e703da6604d57ad12ecf
Dudekula Hazra Bee <dudekulahazrabee@gmail.com>
2026-02-15 23:36:06 +0530

    Update and rename Theory to Theory.java

---
+++ ExceptionHandling/Theory.java
@@ -0,0 +1,3 @@
+### Exception Handling in java
+Exception: An Exception is an unexpected event or problem that arises during the execution of the program, which disrupts the normal flow of instructions.
+  
